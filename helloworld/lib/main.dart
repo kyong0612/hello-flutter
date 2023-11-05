1c57e3cb79cb4a0ca2935cb9f11a383267630fe6
kimkiyong <fenderst0612@gmail.com>
2023-11-05 21:21:04 +0900

    feat: UI

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -66,8 +66,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
+          title: Row(
+        children: const [
+          Icon(Icons.create),
+          Text('初めてのタイトル'),
+        ],
+      )),
       body: Column(
         children: [
           const Text('hello world'),
@@ -97,6 +101,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ]),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print('押したね？')},
+        child: const Icon(Icons.timer),
       ),
     );
   }
