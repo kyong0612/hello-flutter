54d04688b1c84c74d641264474106273e977248f
kimkiyong <fenderst0612@gmail.com>
2023-11-08 23:09:44 +0900

    feat: GPS

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,11 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:helloworld/test_page2.dart';
-import 'package:helloworld/test_page3.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
-import 'test_page1.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,62 +31,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  VideoPlayerController? _controller;
-  final imagePicker = ImagePicker();
+  String _latitude = "NoData";
+  String _longitude = "NoData";
+  String _altitude = "NoData";
+  String _distanceInMeters = "NoData";
+  String _bearing = "Nodata";
 
-  // カメラから動画を取得するメソッド
-  Future getVideoFromCamera() async {
-    XFile? pickedFile = await imagePicker.pickVideo(source: ImageSource.camera);
-    if (pickedFile != null) {
-      _controller = VideoPlayerController.file(File(pickedFile.path));
-      _controller!.initialize().then((_) {
-        setState(() {
-          _controller!.play();
-        });
-      });
+  Future<void> getLocation() async {
+    // 権限を取得
+    LocationPermission permission = await Geolocator.requestPermission();
+    //権限がない場合は戻る
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
     }
-  }
-
-  // ギャラリーから動画を取得するメソッド
-  Future getVideoFromGallery() async {
-    XFile? pickedFile =
-        await imagePicker.pickVideo(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      _controller = VideoPlayerController.file(File(pickedFile.path));
-      _controller!.initialize().then((_) {
-        setState(() {
-          _controller!.play();
-        });
-      });
-    }
+    // 位置情報を取得
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      // 北緯がプラス、南緯がマイナス
+      _latitude = "緯度: ${position.latitude.toStringAsFixed(2)}";
+      //　東経がプラス、西経がマイナス
+      _longitude = "軽度 ${position.longitude.toStringAsFixed(2)}";
+      // 高度
+      _altitude = "高度: ${position.altitude.toStringAsFixed(2)}";
+      // 距離を1000で割ってkmで返す(サンパウロとの距離)
+      _distanceInMeters =
+          "距離: ${(Geolocator.distanceBetween(position.latitude, position.longitude, -23.61, -46.40) / 1000).toStringAsFixed(2)}";
+      // 方位を返す
+      _bearing =
+          "方位: ${(Geolocator.bearingBetween(position.latitude, position.longitude, -23.61, -46.40)).toStringAsFixed(2)}";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-          child: _controller == null
-              ? const Text(
-                  '動画を選択してください',
-                )
-              : VideoPlayer(_controller!)),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          // カメラから取得するボタン
-          FloatingActionButton(
-            onPressed: getVideoFromCamera,
-            child: const Icon(Icons.photo_camera),
-          ),
-          FloatingActionButton(
-            onPressed: getVideoFromGallery,
-            child: const Icon(Icons.photo_album),
-          )
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(_latitude, style: Theme.of(context).textTheme.headlineMedium),
+          Text(_longitude, style: Theme.of(context).textTheme.headlineMedium),
+          Text(_altitude, style: Theme.of(context).textTheme.headlineMedium),
+          Text(_distanceInMeters,
+              style: Theme.of(context).textTheme.headlineMedium),
+          Text(_bearing, style: Theme.of(context).textTheme.headlineMedium),
         ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getLocation,
+        child: const Icon(Icons.location_on),
       ),
     );
   }
